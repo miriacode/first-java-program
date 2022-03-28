8628a4852e548e34cccaa50623e801f072446ad3
miriacode <miriamacunaenciso@gmail.com>
2022-03-28 12:44:34 -0500

    Completed exercise

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name is Miriam");
+        System.out.println("I am 21 years old");
+        System.out.println("My hometown is Lima");
+    }
+
+}
